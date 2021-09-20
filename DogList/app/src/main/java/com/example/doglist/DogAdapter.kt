366ac829c4bdb21b57929688de18145c0fbda201
StefanDN12 <estefan_daniel1002@hotmail.com>
2021-09-20 11:05:54 +0200

    Glide, corrutinas, retrofit2

--- DogList/app/src/main/java/com/example/doglist/DogAdapter.kt
+++ DogList/app/src/main/java/com/example/doglist/DogAdapter.kt
@@ -1,12 +1,10 @@
 package com.example.doglist
 
-<<<<<<< HEAD
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.example.doglist.databinding.ActivityMainBinding
+
 
 class DogAdapter(val  images: List<String>, private val context : Context):RecyclerView.Adapter<DogViewHolder>() {
 
@@ -14,24 +12,11 @@ class DogAdapter(val  images: List<String>, private val context : Context):Recyc
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         return DogViewHolder(layoutInflater.inflate(R.layout.item_dog,parent,false),context)
-=======
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-
-class DogAdapter(val  images: List<String>):RecyclerView.Adapter<DogViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        return DogViewHolder(layoutInflater.inflate(R.layout.item_dog,parent,false))
->>>>>>> main
     }
 
     override fun onBindViewHolder(holder: DogViewHolder, position: Int) {
         val item : String = images[position]
-<<<<<<< HEAD
         holder.bind(item)
-=======
->>>>>>> main
     }
 
     override fun getItemCount(): Int {
